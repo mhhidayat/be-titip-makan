2e8656a2b58c2ba11ca29c70f53ba74124ce433c
mhhidayat <mhhidayat811@gmail.com>
2025-06-01 13:08:20 +0700

    feat(auth): Authentication

--- internal/api/menus.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import "github.com/gofiber/fiber/v2"
-
-func NewMenus(router fiber.Router) {
-	router.Post("/cek-token", func(ctx *fiber.Ctx) error {
-		return ctx.SendString("SUCCESS")
-	})
-}
